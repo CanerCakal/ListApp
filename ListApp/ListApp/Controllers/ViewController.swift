31ff63bdd4c09cab9d09bad0222b54270cd07ce9
Caner ÇAKAL <canercakalofficial@gmail.com>
2026-02-02 21:58:15 +0300

    Table View Added

--- ListApp/ListApp/Controllers/ViewController.swift
+++ ListApp/ListApp/Controllers/ViewController.swift
@@ -7,13 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView : UITableView!
+    
+    let data = ["Swift", "C++", "C", "C#", "Python", "Java", "Ruby", "Go", "PHP"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       /* let cell = UITableViewCell()
+        cell.textLabel?.text = data[indexPath.row]
+        return cell */ // Bu şekil kullanımda tableView da oluşturulan değişiklik olmayacaktı.
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+        // Custom bir cell oluştursaydım cast işlemi yapılması gerekiyordu. (as)
+        
+        cell.textLabel?.text = data[indexPath.row]
+        return cell
+    }
 
 }
 
