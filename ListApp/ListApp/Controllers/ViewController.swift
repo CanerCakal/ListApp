a1fe84d99415955f5ffdaa93244af8238616bb0b
Caner ÇAKAL <canercakalofficial@gmail.com>
2026-02-02 14:53:47 +0300

    ListApp Initialized

---
+++ ListApp/ListApp/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListApp
+//
+//  Created by Caner Çakal on 2.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
