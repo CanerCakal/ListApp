f3864082559e5a913965e5c4d37d8f9271402c55
Caner ÇAKAL <canercakalofficial@gmail.com>
2026-02-04 16:08:51 +0300

    CoreData used

--- ListApp/ListApp/Controllers/ViewController.swift
+++ ListApp/ListApp/Controllers/ViewController.swift
@@ -6,35 +6,21 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
     
     @IBOutlet weak var tableView : UITableView!
     
     var alertController = UIAlertController()
     
-    var data = [String]()
+    var data = [NSManagedObject]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       /* let cell = UITableViewCell()
-        cell.textLabel?.text = data[indexPath.row]
-        return cell */ // Bu şekil kullanımda tableView da oluşturulan değişiklik olmayacaktı.
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
-        // Custom bir cell oluştursaydım cast işlemi yapılması gerekiyordu. (as)
-        
-        cell.textLabel?.text = data[indexPath.row]
-        return cell
+        fetch()
     }
     
     @IBAction func didAddBarButtonItemTapped (_ sender: UIBarButtonItem) {
@@ -60,8 +46,21 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                      isTextFieldAvailable: true) { _ in
             let text = self.alertController.textFields?.first?.text
             if text != "" {
-                self.data.append((text)!)
-                self.tableView.reloadData()
+                
+                let appDelegate = UIApplication.shared.delegate as? AppDelegate
+                
+                let managedObjectContext = appDelegate?.persistentContainer.viewContext
+                
+                let entity = NSEntityDescription.entity(forEntityName: "ListItem",
+                                                        in: managedObjectContext!)
+                
+                let listItem = NSManagedObject(entity: entity!,
+                                               insertInto: managedObjectContext)
+                listItem.setValue(text, forKey: "title")
+                
+                try? managedObjectContext?.save()
+                
+                self.fetch()
             } else {
                 self.presentWarningAlert()
             }
@@ -102,5 +101,86 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         alertController.addAction(cancelButton)
         present(alertController, animated: true, completion: nil)
     }
+    func fetch() {
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        
+        let managedObjectContext = appDelegate?.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "ListItem")
+        
+        data = try! managedObjectContext!.fetch(fetchRequest)
+        
+        tableView.reloadData()
+    }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       /* let cell = UITableViewCell()
+        cell.textLabel?.text = data[indexPath.row]
+        return cell */ // Bu şekil kullanımda tableView da oluşturulan değişiklik olmayacaktı.
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+        // Custom bir cell oluştursaydım cast işlemi yapılması gerekiyordu. (as)
+        
+        let listItem = data[indexPath.row]
+        cell.textLabel?.text = listItem.value(forKey: "title") as? String
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        
+        let deleteAction = UIContextualAction(style: .normal,
+                                              title: "Sil") { _, _, _ in
+            
+            let appDelegate = UIApplication.shared.delegate as? AppDelegate
+            
+            let managedObjectContext = appDelegate?.persistentContainer.viewContext
+            
+            managedObjectContext?.delete(self.data[indexPath.row])
+            
+            try? managedObjectContext?.save()
+            
+            self.fetch()
+            
+            tableView.reloadData()
+        }
+        deleteAction.backgroundColor = .systemRed
+        
+        let editAction = UIContextualAction(style: .normal,
+                                            title: "Düzenle") { _, _, _ in
+            self.presentAlert(title: "Elemanı Düzenle",
+                              message: nil,
+                              defaultButtonTitle: "Düzenle",
+                              cancelButtonTitle: "Vazgeç",
+                              isTextFieldAvailable: true) { _ in
+                let text = self.alertController.textFields?.first?.text
+                if text != "" {
+                    
+                    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+                    
+                    let managedObjectContext = appDelegate?.persistentContainer.viewContext
+                    
+                    self.data[indexPath.row].setValue(text, forKey: "title")
+                    
+                    if managedObjectContext!.hasChanges {
+                        try? managedObjectContext?.save()
+                    }
+                    
+                    
+                    self.tableView.reloadData()
+                } else {
+                    self.presentWarningAlert()
+                }
+            }
+        }
+        
+        let config = UISwipeActionsConfiguration(actions: [deleteAction, editAction])
+        return config
+    }
+}
